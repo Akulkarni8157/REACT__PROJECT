30f3c4d44fb874a774aab6f2f3bdb38edfe7ae70
Atharva Kulkarni <aakulkarni.1011@gmail.com>
2026-02-22 11:58:02 +0530

    AK

---
+++ truck-app/src/calculator/index.jsx
@@ -0,0 +1,16 @@
+
+import addition from "./addition";
+import multiplication from "./multiplication";      
+import subtraction from "./subtraction";
+import division from "./division";  
+
+let calu =() => {
+
+    return (
+        <h1>Calucator app</h1>
+        <addition />
+        <subtraction />
+        <multiplication />
+        <division />
+    )
+}
---
+++ truck-app/src/calculator/multiplication.jsx
@@ -0,0 +1,3 @@
+let mul = (a, b) => {
+    return a * b;
+}
